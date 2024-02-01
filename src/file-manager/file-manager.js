34b4c606e6b0ca05fc86246e1287481e39ad0170
fresemin <fresemin@gmail.com>
2024-02-01 17:01:07 +0300

    feat: fm readline interface, on close

--- src/file-manager/file-manager.js
+++ src/file-manager/file-manager.js
@@ -1,12 +1,13 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 import { getProcessArgument } from '../cli/args.js';
 
 class FileManager {
-
   #username = '';
 
-  constructor() {
+  #rl = null;
 
-  }
+  constructor() { }
 
   // TODO: get username from os
   #welcomeUser() {
@@ -17,8 +18,16 @@ class FileManager {
     console.log(`Welcome to the File Manager, ${this.#username}!`);
   }
 
+  #onRlClose() {
+    console.log(`Thank you for using File Manager, ${this.#username}, goodbye!`);
+  }
+
   start() {
     this.#welcomeUser();
+
+    this.#rl = readline.createInterface({ input, output });
+
+    this.#rl.on('close', this.#onRlClose.bind(this));
   };
 }
 
