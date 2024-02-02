b52659f5c20d344d2377903416d806f9d8818fc7
fresemin <fresemin@gmail.com>
2024-02-02 03:23:13 +0300

    fix: path util get path dir name

--- src/utils/path.util.js
+++ src/utils/path.util.js
@@ -1,5 +1,20 @@
 import path from 'node:path';
 
 export function getPathDirName(dirPath) {
+  const parsedPath = path.parse(dirPath);
+
+  let upperPath = '';
+
+  if ((parsedPath.root === parsedPath.dir) && parsedPath.base !== '') {
+    upperPath = path.join(parsedPath.root);
+    return path.normalize(upperPath);
+  }
+
+  // Would it work on MacOS and Linux?
+  if ((parsedPath.root === parsedPath.dir) && parsedPath.base === '') {
+    upperPath = path.join('/');
+    return path.normalize(upperPath);
+  }
+
   return path.dirname(dirPath);
 }
