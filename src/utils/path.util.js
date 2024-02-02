326e53128ec91d79975d65d2a45381e46a54766b
fresemin <fresemin@gmail.com>
2024-02-02 21:25:01 +0300

    ref: path util using constants

--- src/utils/path.util.js
+++ src/utils/path.util.js
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import { PATH_IN, PATH_EMPTY } from '../constants/constants.js';
 
 export function getPathDirName(dirPath) {
   const parsedPath = path.parse(dirPath);
@@ -10,12 +11,12 @@ export function getPathDirName(dirPath) {
   let upperPath = '';
 
   // Would it work for MacOS and Linux?
-  if ((isRoot) && pathBase !== '' && pathBase !== '.') {
+  if ((isRoot) && pathBase !== PATH_EMPTY && pathBase !== PATH_IN) {
     upperPath = path.join(parsedPath.root);
     return path.normalize(upperPath);
   }
 
-  if ((isRoot) && (pathBase === '' || pathBase === '.')) {
+  if ((isRoot) && (pathBase === PATH_EMPTY || pathBase === PATH_IN)) {
     upperPath = path.join('/');
     return path.normalize(upperPath);
   }
