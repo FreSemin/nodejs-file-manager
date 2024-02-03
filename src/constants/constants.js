2321cca89d59128e6ee3750b697220873b2b1e78
fresemin <fresemin@gmail.com>
2024-02-03 20:50:27 +0300

    feat: error invalid input text constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,5 +4,6 @@ export const PATH_UP = '..';
 export const PATH_EMPTY = '';
 
 export const ERROR_OPERATION_FAIL_TEXT = 'Operation failed';
+export const ERROR_INVALID_INPUT_TEXT = 'Invalid Input';
 
 export const USERNAME_ARG = 'username';
