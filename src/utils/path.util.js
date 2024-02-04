15cd8175f05456abf62eeecf69d4ed58b3403ea2
fresemin <fresemin@gmail.com>
2024-02-04 21:18:54 +0300

    feat: get relative or absolute destination path util

--- src/utils/path.util.js
+++ src/utils/path.util.js
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import { ROOT_DIR, PATH_IN, PATH_EMPTY } from '../constants/constants.js';
-import { InvalidInputError } from './errors.util.js';
+import { InvalidInputError, OperationFailedError } from './errors.util.js';
+import { access } from 'node:fs/promises';
 
 export function getUpDirPath(dirPath) {
   return path.dirname(dirPath);
@@ -31,4 +32,24 @@ export function parseLineArgs(inputStr) {
   const args = Array.from(normalizedStr.match(regexp));
 
   return args.map((arg) => arg.replaceAll('\"', ''));
+}
+
+export async function getRelativeOrAbsoluteDestinationPath(currentDirPath, destinationPath) {
+  try {
+    const relativePath = path.normalize(path.join(currentDirPath, destinationPath));
+
+    await access(relativePath);
+
+    return relativePath;
+  } catch {
+    try {
+      const absolutePath = path.normalize(path.join(destinationPath));
+
+      await access(absolutePath);
+
+      return absolutePath;
+    } catch {
+      throw new OperationFailedError();
+    }
+  }
 }
