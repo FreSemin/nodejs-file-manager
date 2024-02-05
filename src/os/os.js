114edb8cf22f9a6cfd3d80ba3388f4803e8981f4
fresemin <fresemin@gmail.com>
2024-02-05 03:41:24 +0300

    feat: os perform operation

--- src/os/os.js
+++ src/os/os.js
@@ -1,4 +1,5 @@
 import os from 'node:os';
+import { InvalidInputError } from '../utils/errors.util.js';
 
 export function getUserHomeDir() {
   return os.homedir();
@@ -6,4 +7,22 @@ export function getUserHomeDir() {
 
 export function getEOL() {
   return os.EOL;
+}
+
+export async function performOSOperation(argument) {
+  const normalizedArg = argument.toLowerCase();
+
+  switch (normalizedArg) {
+    case '--eol': {
+      const eol = getEOL();
+
+      console.log(JSON.stringify(eol));
+
+      break;
+    }
+
+    default: {
+      throw new InvalidInputError();
+    }
+  }
 }
