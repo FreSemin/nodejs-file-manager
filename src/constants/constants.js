da3b51481475ee8ef78578314d1a2f1c38d00465
fresemin <fresemin@gmail.com>
2024-02-05 20:57:47 +0300

    ref: delete unused constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,3 @@
-export const ROOT_DIR = '/';
-export const PATH_IN = '.';
-export const PATH_UP = '..';
-export const PATH_EMPTY = '';
-
 export const ERROR_OPERATION_FAIL_TEXT = 'Operation failed';
 export const ERROR_INVALID_INPUT_TEXT = 'Invalid Input';
 
